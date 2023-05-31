3bc3404adc6f621357f906b4eb3e01afad4b0213
fnmarcos <127150588+fnmarcos@users.noreply.github.com>
2023-05-31 00:28:56 -0300

    Add files via upload

--- js/javascript_login.js
+++ js/javascript_login.js
@@ -9,10 +9,7 @@ while (Number.isInteger(var_edad) == false) {
 
 verificacion_edad(var_edad);
 
-var cod_postal = prompt("Ingrese el codigo postal de residencia:");
-cod_postal = parseInt(cod_postal);
-
-ubicacion(cod_postal)
+estudiante();
 
 // Funciones ------------------------------------------------------------------
 
@@ -25,13 +22,16 @@ function verificacion_edad(edad) {
     }
 }
 
-function ubicacion(codigo_postal) {
+function estudiante() {
 
-    if (codigo_postal == 2900) {
-        alert("Usted se encuentra en San Nicolás. Usted posee un 20% de descuento en todos nuestros cursos y capacitaciones.");
+    let nombre = prompt("Ingrese su nombre a continuacion:")
+    let confirmacion = confirm("¿Es estudiante universitario?")
+
+    if (confirmacion == true) {
+        alert(nombre + ", usted posee un 20% de descuento en todos nuestros cursos y capacitaciones.");
         window.location = "./index.html";
     } else {
-        alert("Usted se encuentra fuera de San Nicolás.");
+        alert(nombre + ", le informamos que contamos con cursos y capacitaciones a excelentes precios.");
         window.location = "./index.html";
     }
 }
